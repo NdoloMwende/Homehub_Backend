2a247e7b18c188412b33ba43a36832643ceacb41
LabanMugutu <labanmugutu@gmail.com>
2026-01-13 18:39:28 +0300

    Updates routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,15 +1,13 @@
-from flask import Blueprint
-
-# Import all route blueprints
-from routes.auth import auth_bp
-from routes.users import users_bp
-from routes.properties import properties_bp
-from routes.units import units_bp
-from routes.leases import leases_bp
-from routes.rent_invoices import rent_invoices_bp
-from routes.payments import payments_bp
-from routes.maintenance import maintenance_bp
-from routes.notifications import notifications_bp
+from .auth import auth_bp
+from .users import users_bp
+from .properties import properties_bp
+from .units import units_bp
+from .leases import leases_bp
+from .rent_invoices import rent_invoices_bp
+from .payments import payments_bp
+from .maintenance import maintenance_bp
+from .notifications import notifications_bp
+from .upload import upload_bp  # 👈 Added this line
 
 __all__ = [
     'auth_bp',
@@ -20,5 +18,6 @@ __all__ = [
     'rent_invoices_bp',
     'payments_bp',
     'maintenance_bp',
-    'notifications_bp'
+    'notifications_bp',
+    'upload_bp' # 👈 Added this line
 ]
